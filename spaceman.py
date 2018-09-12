3741036333fdaeb4cd5710d1eeb58540839390d8
j-n4m4573 <jg_220@yahoo.com>
2018-09-12 14:54:36 -0700

    upgraded word search

--- spaceman.py
+++ spaceman.py
@@ -1,6 +1,6 @@
 
 from turtle import *
-
+import re
 ## If the guessed letter is in the mystery word, the position(s)
 ## of the letter(s) are revealed in the placeholders.
 ## If a guessed letter occurs more than once in the word all the places that letter occurs are revealed.
@@ -17,7 +17,8 @@ from turtle import *
 ## if letter is not in mystery word then display on screen and add to other
 ## choice listself.
 ## repeat ask for letter and check to see if letter is in the mystery word
-## Game finishes when user reaches seven missed letters
+## Game finishes when user reaches seven missed letters or user guesses the
+# complete word
 
 mystery_word = "mystery"
 user_guesses = []
@@ -27,23 +28,27 @@ counter = 0
 rev_guess = ['_','_','_','_','_','_','_']
 
 def choose_letter():
-    user_guess = input("Pick a letter... guess the mystery word (7 letters)  _ _ _ _ _ _ _  ")
-    checkGuess(user_guess)
+    user_guess = input("Pick a letter... guess the mystery word ({} letters)".format(len(mystery_word)))
+    if type(user_guess) is str and len(user_guess) == 1:
+        checkGuess(user_guess)
 
 def checkGuess(user_guess):
     for letter in mystery_word:
         if user_guess == letter:
-            rev_guess[mystery_word.find("{}".format(letter))] = "{}".format(letter)
+            temp_list = [word.start() for word in re.finditer(letter, mystery_word)]
+            for index in temp_list:
+            # rev_guess[mystery_word.find("{}".format(letter))] = "{}".format(letter)
+                rev_guess[index] = "{}".format(letter)
             print(" ".join(rev_guess))
             nice_guess.append(user_guess)
             user_guesses.append(user_guess)
             print("Goood choice buddy!! {} is on our list ".format(user_guess))
             gameEngine()
     else:
-        wrong_guess.append(user_guess)
-        print("sorry buddy,{} is not on our list".format(user_guess))
-        print(wrong_guess)
-        gameEngine()
+            wrong_guess.append(user_guess)
+            print("sorry buddy,{} is not on our list".format(user_guess))
+            print(wrong_guess)
+            gameEngine()
 
 def gameEngine():
     keep_it_going = True
